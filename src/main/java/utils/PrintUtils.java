3f9e989b9d28fa7b109c7823a080041a3409a606
honghyeong <scarletgold0601@gmail.com>
2021-11-27 16:57:59 +0900

    refactor: printUtil 음수 예외처리 추가

--- src/main/java/utils/PrintUtils.java
+++ src/main/java/utils/PrintUtils.java
@@ -7,13 +7,14 @@ public class PrintUtils {
     }
 
     public static void printResult(int ballCount, int strikeCount){
-        if (strikeCount == 3) {
+        if(ballCount<0 || strikeCount<0){
+            throw new IllegalArgumentException();
+        }
+        else if (strikeCount == 3) {
             System.out.println("3스트라이크");
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
-
         } else if (ballCount + strikeCount == 0) {
             System.out.println("낫싱");
-
         } else {
             String a = "";
             if (ballCount > 0) {
